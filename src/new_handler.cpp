0ebb06f9d6db131c3b2bc4c8bf81f75aa76598ad
Krzysztof Debski <krzysztof@debski.eu>
2024-10-30 21:50:22 +0100

    Update new_handler.cpp
    Make a workaround to make it work with arduino ide

--- src/new_handler.cpp
+++ src/new_handler.cpp
@@ -19,7 +19,8 @@
 
 #include "new"
 
-const std::nothrow_t std::nothrow = { };
+// Commit to make it work with Arduino IDE
+// const std::nothrow_t std::nothrow = { };
 
 //Name selected to be compatable with g++ code
 std::new_handler __new_handler;
